2fddc6462757c2ff8e0c3b7805dba7223d7bb440
samev <samevangelista07@icloud.com>
2024-10-17 13:09:57 -0500

    Created Adv Dev Reference Sheet

---
+++ Intro-to-Adv-Dev/app.js
@@ -0,0 +1,35 @@
+// Variables
+// Variables are containers for storing data values.
+// Variables can be strings (text), numbers, or booleans (true/false).
+
+
+// Can print things out using node file and console.log
+
+
+// Numbers can be added, subtracted, multiplied, and divided.
+
+
+// Conditionals
+
+
+
+// Comparison Operators
+
+
+
+// Logical Operators
+
+
+
+
+// Loops
+
+
+
+
+// Functions
+
+
+
+
+// Arrays
